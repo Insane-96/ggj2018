4bfef9ed1c699c018025d05ec80b9165b34bbecf
Alberto Del Villano <insane96mcp@gmail.com>
2018-01-26 21:49:46 +0100

    Added Camera Interface

---
+++ Assets/Sources/Interfaces/ICamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICameraControl
+{
+    /// <summary>
+    /// Changes what the camera is looking at
+    /// </summary>
+    /// <param name="gameObject">The GameObject the camera will start looking at</param>
+    void LookAt(GameObject gameObject);
+}

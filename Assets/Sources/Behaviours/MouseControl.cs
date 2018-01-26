1dd9c76403444c566004cb56b4550ed7d97f3f1d
Alberto Del Villano <insane96mcp@gmail.com>
2018-01-27 00:25:21 +0100

    Added Mouse Control. Missing Movement

---
+++ Assets/Sources/Behaviours/MouseControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour, IPlayableCharacter
+{
+    private bool isSelected = false;
+    public ICameraControl cameraControl;
+    public GameObject player;
+    private IPlayableCharacter playableCharacter;
+
+    public float movementSpeed;
+
+    public void Select()
+    {
+        playableCharacter = player.GetComponent<IPlayableCharacter>();
+        isSelected = true;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (!isSelected)
+            return;
+
+        //TODO Movements
+    }
+
+    private void ReturnToPlayer()
+    {
+        isSelected = false;
+        playableCharacter.Select();
+        cameraControl.LookAt(player);
+    }
+}

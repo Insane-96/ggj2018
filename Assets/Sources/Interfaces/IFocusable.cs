6df76345ccc865eb377b6d04581a5f57f12dee4c
Alberto Del Villano <insane96mcp@gmail.com>
2018-01-26 23:53:47 +0100

    Added IPlayableCharacter

--- Assets/Sources/Interfaces/IFocusable.cs
+++ Assets/Sources/Interfaces/IFocusable.cs
@@ -5,23 +5,17 @@ using UnityEngine;
 /// <summary>
 /// Interface to apply to entities that can be transfered to by the player and then controlled 
 /// </summary>
-public interface IControllable {
+public interface IFocusable
+{
     /// <summary>
     /// When the player has this object focused and can select it. 
     /// </summary>
-    /// <returns>Returns true if the focus was succesfull</returns>
-    bool Focus();
+    void Focus();
 
     /// <summary>
     /// De-selects the object when the player is in focus.
     /// </summary>
-    /// <returns>Returns true if the unfocus was succesfull</returns>
-    bool Unfocus();
-
-    /// <summary>
-    /// Called to select the object and take control of it
-    /// </summary>
-    void Select();
+    void Unfocus();
 
     /// <summary>
     /// Called to deselect the object and return to player control

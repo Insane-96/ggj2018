6ce608cebe2b9f441f02bcfd00f8c2088f84d44f
aleaiudi <aleaiudi1@hotmail.it>
2018-01-27 01:06:40 +0100

    added patientcontroller

---
+++ Assets/Sources/Behaviours/PatientController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+    //crea un overlavsphere ogni click di un tasto e se dentro l'overlap ci sono enemy attiva l 'interfaccia Inoisedetected
+   /* private void MakeNoise()
+    {
+        Collider[] cols = Physics.OverlapSphere(transform.position, noiseRadius);
+        foreach (Collider col in cols)
+        {
+            IListener Ilistener = col.GetComponent<IListener>();
+            if (Ilistener == null)
+                continue;
+
+            Ilistener.Listen(transform.position);
+        }
+    }*/
+
+}
